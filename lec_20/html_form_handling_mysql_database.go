2a27c61b0f44c1b6c40df4b33acf2f03748c3376
rafi ahamed <cityinstitute1992@gmail.com>
2021-03-17 03:17:48 +0600

    basic method of postman

--- lec_20/html_form_handling_mysql_database.go
+++ lec_20/html_form_handling_mysql_database.go
@@ -1,32 +1,42 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 	"html/template"
 	"net/http"
+	"os"
 
-	_ "github.com/go-sql-driver/mysql"
+	"github.com/mateors/mcb"
 )
 
-var db *sql.DB
+//var db *sql.DB
+var db *mcb.DB
 var err error
 
 func init() {
 	// Open up our database connection.
 	// I've set up a database on my local machine using phpmyadmin.
 	// The database is called testDb
-	db, err = sql.Open("mysql", "root:ahammed80223@tcp(127.0.0.1:3306)/hosting_db")
+	//db, err = sql.Open("mysql", "root:ahammed80223@tcp(127.0.0.1:3306)/hosting_db")
 
 	// if there is an error opening the connection, handle it
-	if err != nil {
-		panic(err.Error())
-	}
+	//if err != nil {
+	//panic(err.Error())
+	//}
 
 	// defer the close till after the main function has finished
 	// executing
 	//  defer db.Close()
-	fmt.Println("db connection successful")
+	//fmt.Println("db connection successful")
+
+	db = mcb.Connect("localhost", "rafi", "ahammed80223")
+	res, err := db.Ping()
+	if err != nil {
+		fmt.Println(res)
+		os.Exit(1)
+
+	}
+	fmt.Println(res, err)
 
 }
 
@@ -82,37 +92,65 @@ func docs(w http.ResponseWriter, r *http.Request) {
 	//fmt.Fprintf(w, `welcome`)
 }
 
-func request(w http.ResponseWriter, r *http.Request) {
-	//r.ParseForm()
-	//method1:
+//func request(w http.ResponseWriter, r *http.Request) {
+//r.ParseForm()
+//method1:
 
-	name := r.FormValue("name")
-	company := r.FormValue("company")
-	email := r.FormValue("email")
-	//fmt.Println(name, company, email)
+//name := r.FormValue("name")
+//company := r.FormValue("company")
+//email := r.FormValue("email")
+//fmt.Println(name, company, email)
 
-	//fmt.Fprintf(w, `received %s %s %s`, name, company, email) //response
+//fmt.Fprintf(w, `received %s %s %s`, name, company, email) //response
 
-	//method2:
-	/*
-		r.ParseForm()
+//method2:
+/*
+	r.ParseForm()
 
-		for key, val := range r.Form {
+	for key, val := range r.Form {
 
-			fmt.Println(key, val)
-		}
-	*/
-	qs := "INSERT INTO `request` (`id`, `name`, `company`, `email`, `status`) VALUES (NULL,' %s',' %s', '%s', '1');"
-	sql := fmt.Sprintf(qs, name, company, email)
-
-	fmt.Println(sql)
-	insert, err := db.Query(sql)
-	//if there is an error opening the connection, handle it
-	if err != nil {
-		panic(err.Error())
+		fmt.Println(key, val)
 	}
-	defer insert.Close()
+*/
+//qs := "INSERT INTO `request` (`id`, `name`, `company`, `email`, `status`) VALUES (NULL,' %s',' %s', '%s', '1');"
+//sql := fmt.Sprintf(qs, name, company, email)
 
-	fmt.Fprintf(w, `ok`)
+//fmt.Println(sql)
+//insert, err := db.Query(sql)
+//if there is an error opening the connection, handle it
+//if err != nil {
+//	panic(err.Error())
+//}
+//defer insert.Close()
 
+//fmt.Fprintf(w, `ok`)
+
+//}
+
+type RequestTable struct {
+	ID      string `json:"aid"`
+	Name    string `json:"name"`
+	Email   string `json:"email"`
+	Company string `json:"company"`
+	Type    string `json:"type"`
+	Status  int    `json:"status"`
+}
+
+func request(w http.ResponseWriter, r *http.Request) {
+	//Method2
+	r.ParseForm()
+	for key, val := range r.Form {
+		fmt.Println(key, val)
+	}
+
+	var reqTable RequestTable
+
+	r.Form.Add("bucket", "master_academy")
+	r.Form.Add("aid", "request::6") //we will update later
+	r.Form.Add("type", "request")
+	r.Form.Add("status", "1")
+	pRes := db.Insert(r.Form, &reqTable)
+	fmt.Println(pRes.Status, pRes.Errors)
+
+	fmt.Fprintf(w, `OK`)
 }
