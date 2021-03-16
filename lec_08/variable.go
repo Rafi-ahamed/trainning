2791f0524d464035e37891e74db4f409fa215fdb
rafi ahamed <cityinstitute1992@gmail.com>
2021-03-16 23:06:56 +0600

    go method

---
+++ lec_08/variable.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+
+func main( ){
+
+//var name string="rafi"
+//var name2 ,city string="rafi","dhaka"
+
+//fmt.Println(name,city,name2)
+
+//} 
+agevar result float32
+/*
+var name string="rafi"
+var name2 ,city string="rafi","dhaka"
+
+fmt.Println(name,city,name2)
+*/
+//char
+var age int = 10
+var result float32=50.95
+
+fmt.Println(age,result)
+
+}
